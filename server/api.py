dcabe41930d74e375bbfb9d6c2a0f1859e4ebc74
longvu2907 <74065518+longvu2907@users.noreply.github.com>
2021-12-14 22:36:57 +0700

    comment code

--- server/api.py
+++ server/api.py
@@ -5,7 +5,6 @@ from datetime import datetime
 def getAPIData():
   #get country data
   data = {}
-
   response = requests.get('https://coronavirus-19-api.herokuapp.com/countries').json()
   for country in response:
     name = country['country']
@@ -16,11 +15,11 @@ def getAPIData():
       "deaths" : country['deaths'],
       "provinces": {}
     }
-  #get province data
+
+  #get vietnam province data
   req = requests.get("https://vi.wikipedia.org/wiki/Th%E1%BA%A3o_lu%E1%BA%ADn_B%E1%BA%A3n_m%E1%BA%ABu:D%E1%BB%AF_li%E1%BB%87u_%C4%91%E1%BA%A1i_d%E1%BB%8Bch_COVID-19/S%E1%BB%91_ca_nhi%E1%BB%85m_theo_t%E1%BB%89nh_th%C3%A0nh_t%E1%BA%A1i_Vi%E1%BB%87t_Nam")
   soup = BeautifulSoup(req.text, 'html.parser')
   provinceData = {}
-
   table = soup.find("table", "wikitable")
   trs = table.find_all("tr")
   i = 0
